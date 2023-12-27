3aabcd12f2793f7d2313a3aa8a9ed5ef646fb5cf
abhikrishnaram <abhikrishnaram88@gmail.com>
2023-12-27 22:50:59 +0530

    build commit

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("user.urls")),
     path("", include("project.urls")),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [path("api/", include(urlpatterns))]
@@ -31,8 +31,3 @@ urlpatterns = [path("api/", include(urlpatterns))]
 # get all projects by category
 # get project by id
 # get all teams by mentor - admin and mentor only
-
-
-
-
-
